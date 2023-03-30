8b56befc79bfc11ad364aa642fc7d97b499f3d48
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2023-03-30 16:26:13 +0600

    load all data and specific data by id from server

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 require('dotenv').config();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ykgatkn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -18,11 +18,20 @@ async function run() {
         await client.connect();
         const productsCollection = client.db('manufacturer').collection('products')
 
+        //Get All Products
         app.get('/products', async (req, res) => {
             const query = {};
             const cursor = productsCollection.find(query);
             const products = await cursor.toArray();
             res.send(products)
+        })
+
+        // Get A Specific Product Detail
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            res.send(product);
         })
 
     } finally {
